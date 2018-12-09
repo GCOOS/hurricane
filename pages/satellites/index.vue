86b50d11b2142a0c2c434261a41075341eafbf22
skobara_MBP <shin.kobara@gmail.com>
2018-12-09 16:04:45 -0600

    change column

--- pages/satellites/index.vue
+++ pages/satellites/index.vue
@@ -7,16 +7,8 @@
             <b-row>
                 <b-col>
                     <b-card-group deck>
-                        <b-card sub-title="Atlantic & Gulf of Mexico - GeoColor" img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-GEOCOLOR-250x250.gif" img-top><a href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-GEOCOLOR-1000x1000.gif" target="_blank"><i class="fas fa-download"></i>This image</a>
-                        </b-card>
+                        <b-card sub-title="Atlantic & Gulf of Mexico - GeoColor" img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-GEOCOLOR-250x250.gif" img-top><a href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-GEOCOLOR-1000x1000.gif" target="_blank"><i class="fas fa-download"></i></a></b-card>
                         <b-card sub-title="Evolution GOES16-CONUS-02" img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-02-250x250.gif" img-top><a href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-02-1000x1000.gif" target="_blank"><i class="fas fa-download"></i></a></b-card>
-                    </b-card-group>
-                </b-col>
-            </b-row>
-            <br>
-            <b-row>
-                <b-col>
-                    <b-card-group deck>
                         <b-card sub-title="Temperature GOES16-CONUS-07" img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-07-250x250.gif" img-top><a href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-07-1000x1000.gif" target="_blank"><i class="fas fa-download"></i></a></b-card>
                         <b-card sub-title="Water Vapor GOES16-CONUS-08" img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-08-250x250.gif" img-top>
                             <a href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-08-1000x1000.gif" target="_blank"><i class="fas fa-download"></i></a></b-card>

fd141e9b2d5318eecb3affe0835013f914b0ff45
skobara_MBP <shin.kobara@gmail.com>
2018-12-12 15:40:47 -0600

    disable conu-07

--- pages/satellites/index.vue
+++ pages/satellites/index.vue
@@ -1,23 +1,67 @@
 <template>
-    <div class="canvas">
-        <b-container fluid>
-            <div class="centered">
-                <h2>Satellites</h2>
-            </div>
-            <b-row>
-                <b-col>
-                    <b-card-group deck>
-                        <b-card sub-title="Atlantic & Gulf of Mexico - GeoColor" img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-GEOCOLOR-250x250.gif" img-top><a href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-GEOCOLOR-1000x1000.gif" target="_blank"><i class="fas fa-download"></i></a></b-card>
-                        <b-card sub-title="Evolution GOES16-CONUS-02" img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-02-250x250.gif" img-top><a href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-02-1000x1000.gif" target="_blank"><i class="fas fa-download"></i></a></b-card>
-                        <b-card sub-title="Temperature GOES16-CONUS-07" img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-07-250x250.gif" img-top><a href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-07-1000x1000.gif" target="_blank"><i class="fas fa-download"></i></a></b-card>
-                        <b-card sub-title="Water Vapor GOES16-CONUS-08" img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-08-250x250.gif" img-top>
-                            <a href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-08-1000x1000.gif" target="_blank"><i class="fas fa-download"></i></a></b-card>
-                        <b-card sub-title="Top Height GOES16-CONUS-13" img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-13-250x250.gif" img-top><a href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-13-1000x1000.gif" target="_blank"><i class="fas fa-download"></i></a></b-card>
-                    </b-card-group>
-                </b-col>
-            </b-row>
-        </b-container>
-    </div>
+  <div class="canvas">
+    <b-container fluid>
+      <div class="centered">
+        <h2>Satellites</h2>
+      </div>
+      <b-row>
+        <b-col>
+          <b-card-group deck>
+            <b-card
+              sub-title="Atlantic & Gulf of Mexico - GeoColor"
+              img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-GEOCOLOR-250x250.gif"
+              img-top
+            >
+              <a
+                href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-GEOCOLOR-1000x1000.gif"
+                target="_blank"
+              >
+                <i class="fas fa-download"></i>
+              </a>
+            </b-card>
+            <b-card
+              sub-title="Evolution GOES16-CONUS-02"
+              img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-02-250x250.gif"
+              img-top
+            >
+              <a
+                href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-02-1000x1000.gif"
+                target="_blank"
+              >
+                <i class="fas fa-download"></i>
+              </a>
+            </b-card>
+            <!--                        <b-card sub-title="Temperature GOES16-CONUS-07" img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-07-250x250.gif" img-top><a href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-07-1000x1000.gif" target="_blank"><i class="fas fa-download"></i></a></b-card>
+            -->
+            <b-card
+              sub-title="Water Vapor GOES16-CONUS-08"
+              img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-08-250x250.gif"
+              img-top
+            >
+              <a
+                href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-08-1000x1000.gif"
+                target="_blank"
+              >
+                <i class="fas fa-download"></i>
+              </a>
+            </b-card>
+            <b-card
+              sub-title="Top Height GOES16-CONUS-13"
+              img-src="https://geo.gcoos.org/data/hurricane/GOES16-GM-13-250x250.gif"
+              img-top
+            >
+              <a
+                href="https://cdn.star.nesdis.noaa.gov/GOES16/ABI/GIFS/GOES16-GM-13-1000x1000.gif"
+                target="_blank"
+              >
+                <i class="fas fa-download"></i>
+              </a>
+            </b-card>
+          </b-card-group>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 

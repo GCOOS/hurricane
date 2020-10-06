3f7fd960650e6c8479bd4b5798e23684338267ef
Shin Kobara <shin.kobara@gmail.com>
2020-10-06 12:47:49 -0500

    add vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ module.exports = {
     ],
     script: [{
         /* jQuery for ajax */
-        src: "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"
+        src: "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
       },
       {
         src: "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"
@@ -88,7 +88,6 @@ module.exports = {
    ** Nuxt.js modules
    */
   modules: [
-    "@nuxtjs/sentry",
     // cache with options
     [
       "@nuxtjs/component-cache",

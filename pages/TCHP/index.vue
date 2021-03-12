97e5bed0bf0ce056829cf3c84b70b1891db9925b
kojiroh.homma@gmail.com <shin.kobara@gmail.com>
2021-03-12 15:16:34 -0600

    add TCHP page

---
+++ pages/TCHP/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="canvas">
+    <b-container fluid>
+      <iframe
+        src="https://tamu.maps.arcgis.com/apps/webappviewer/index.html?id=b1fdb93e60784c0eb9fa59d3dd102ae7"
+        title="GCOOS Tropical Cyclone Heat Potential (TCHP) in the Gulf of Mexico"
+        style="min-height:80vh;"
+        height="100%"
+        width="100%"
+        allowfullscreen="true"
+      ></iframe>
+    </b-container>
+  </div>
+</template>
